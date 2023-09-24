783b71a2829e89b8e2df60695d468432a260292e
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2023-09-24 15:53:43 +0000

    feat: defaulting mongo uri

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,7 +1,7 @@
 import { Db, MongoClient } from 'mongodb';
 
 export async function getDb(): Promise<Db>{
-  const uri = 'mongodb://localhost:27017/mydatabase';
+  const uri = process.env['MONGO_URI'] ?? 'mongodb://localhost:27017/mydatabase';
   const client = new MongoClient(uri);
   await client.connect();
   const database = client.db('mydatabase');
