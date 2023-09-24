95bf196b115c327804185fa3124a427164fa8fef
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2023-09-24 16:14:23 +0000

    fix: mongodb uri

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,7 +1,7 @@
 import { Db, MongoClient } from 'mongodb';
 
 export async function getDb(): Promise<Db>{
-  const uri = process.env['MONGO_URI'] ?? 'mongodb://localhost:27017/mydatabase';
+  const uri = process.env['MONGODB_URI'] ?? 'mongodb://localhost:27017/mydatabase';
   const client = new MongoClient(uri);
   await client.connect();
   const database = client.db('tutorial-todo');
