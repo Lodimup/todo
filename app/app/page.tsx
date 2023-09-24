13ce1cd418c27de91d6b7ec1bf9f8d98718d963c
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2023-09-24 16:46:44 +0000

    feat: pretty layout

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -4,12 +4,12 @@ import { TodoCards } from "@/components/cards/todo-cards";
 export default function Page() {
   return (
     <div
-      className="min-h-screen min-w-screen"
+      className="min-h-screen min-w-screen flex justify-center bg-gray-100 p-4"
     >
-    <div className="flex space-x-2">
-      <NewTodoCard />
-      <TodoCards />
-    </div>
+      <div className="flex space-x-2">
+        <NewTodoCard />
+        <TodoCards />
+      </div>
     </div>
   )
 }
--- app/components/cards/new-todo-card/index.tsx
+++ app/components/cards/new-todo-card/index.tsx
@@ -5,13 +5,13 @@ import { SubmitBtn } from "./submit-btn.component"
 
 export const NewTodoCard = () => {
   return (
-    <Card className="w-[300px] min-h-screen">
+    <Card className="w-[300px] h-[300px]">
       <CardHeader><CardTitle>New todo</CardTitle></CardHeader>
       <CardContent>
         <form action={newTodo} className="flex flex-col space-y-2">
-          <Input name="title" placeholder="title"/>
-          <Input name="content" placeholder="content"/>
-          <SubmitBtn/>
+          <Input name="title" placeholder="title" />
+          <Input name="content" placeholder="content" />
+          <SubmitBtn />
         </form>
       </CardContent>
     </Card>
